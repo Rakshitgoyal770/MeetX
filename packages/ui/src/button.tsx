9520cf4170bef16396e00922b14d815c729a551e
RakshitGoyal770 <rakshitgoyal770@gmail.com>
2026-03-22 02:26:33 +0530

    signin and signup page

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,17 +2,31 @@
 
 import { ReactNode } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+const Varient = {
+  primary: "bg-blue-500 text-white hover:bg-blue-600",
+  secondary: "bg-gray-500 text-white hover:bg-gray-600",
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const Size = {
+  small : "px-2 py-1 text-sm",
+  medium : "px-4 py-2",
+  onClick: () => void 0,
+  large : "px-6 py-3 text-lg"
+}
+
+interface ButtonProps {
+  varient: keyof typeof Varient;
+  size: keyof typeof Size;
+  className?: string;
+  children?: ReactNode;
+  onClick?: () => void;
+}
+
+export const Button = ({ className, varient, size, children , onClick}: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={`${Varient[varient]} ${Size[size]} ${className || ""}`}
+      onClick={onClick}
     >
       {children}
     </button>
